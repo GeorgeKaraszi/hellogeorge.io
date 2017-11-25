119377b233e39ff96f5b33a9ba2e0ab7bbd68a03
George Karaszi <georgekaraszi@gmail.com>
2017-11-25 15:37:33 -0500

    Icon packs for timeline

--- src/components/timeline/LineFooter.vue
+++ src/components/timeline/LineFooter.vue
@@ -3,9 +3,6 @@
     <div class="section-inner">
 
       <div class="good-bye">
-        <p>That's all, buddy!</p>
-        <p>Psst. You can click the icons to scroll to each post. Click the last one to scroll back up.</p>
-
       </div>
     </div>
     <!-- section-inner -->
